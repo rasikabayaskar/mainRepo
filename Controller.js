ef240eb6749ef6bd89ae6d6bbad44fbfd4dec35c
rasikabayaskar <bayaskarrasika@gmail.com>
2017-04-26 00:17:27 +0530

    Add files via upload
    final changes

--- Controller.js
+++ Controller.js
@@ -2,12 +2,39 @@ var app = angular.module('jsbin', []);
 
 app.controller('DemoCtrl', function($scope,$http) {
   
-	   $scope.city = null;
-	  var URL = 'http://api.openweathermap.org/data/2.5/forecast';
-  
-$scope.getUpdate =  function()
+  $scope.city = null;
+  $scope.isCelcius = false;
+  $scope.showConverter = false;
+ var URL = 'http://api.openweathermap.org/data/2.5/forecast';
+  $scope.getScale =  function(input)
 	{
-		
+
+	var tableLength = document.getElementById("displayTable").rows.length;
+	  if($scope.isCelcius == true)
+	   {
+			for(var i=1; i< tableLength; i++)
+			{
+				var show = document.getElementById("displayTable").rows[i].cells[0].innerHTML;
+				var tempToFaren= parseInt(show,10) + 32 + String.fromCharCode(176) +"F";
+				document.getElementById("displayTable").rows[i].cells[0].innerHTML = tempToFaren;
+			}
+			$scope.isCelcius = false;
+	   }
+	   else
+	   {
+			for(var i=1; i< tableLength; i++)
+			{
+				var show = document.getElementById("displayTable").rows[i].cells[0].innerHTML;
+				var tempToFaren= parseInt(show,10) - 32 + String.fromCharCode(176) +"C";
+				document.getElementById("displayTable").rows[i].cells[0].innerHTML = tempToFaren;
+			}
+			$scope.isCelcius = true;
+	   }
+}
+  	$scope.getUpdate =  function()
+	{
+		$scope.showConverter = true;
+		$scope.isCelcius = true;
 				  var request = {
 					method: 'GET',  
 				url: URL,  
@@ -38,6 +65,21 @@ $scope.getUpdate =  function()
 						 
 					});
 		}
+		
+		$scope.getUpdateFromEnter = function(keyEvent)
+	{
+		if (keyEvent.which === 13 )
+		{
+			$scope.getUpdate();
+		}
+
+	}
 	
-	
+});
+app.filter('convertToCel',function(){
+return function( input ) {
+var cel = input - 273.15;
+var cel = cel.toFixed(2) +String.fromCharCode(176) + "C";
+return cel;
+}
 });
